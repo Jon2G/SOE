a4ba5c9d864192695d5ec072dc6c7c6288045871
Jonathan Eduardo Garcia Garcia <jonyjouna@gmail.com>
2021-02-11 21:47:39 -0600

    Prototipo barra lateral

--- OrganizadorEscolar/OrganizadorEscolar/Views/AboutPage.xaml.cs
+++ OrganizadorEscolar/OrganizadorEscolar/Views/AboutPage.xaml.cs
@@ -7,9 +7,65 @@ namespace OrganizadorEscolar.Views
 {
     public partial class AboutPage : ContentPage
     {
+        bool _isFlyoutOpen = false;
+        private const uint _flyoutSpeed = 200;
+        private const uint Scale =10;
+        private const uint HalfScale =Scale/2;
+
         public AboutPage()
         {
             InitializeComponent();
+        }
+        void OnToggleMenuOpen(object sender, EventArgs e)
+        {
+            if (_isFlyoutOpen)
+            {
+                return;
+            }
+            _isFlyoutOpen = true;
+            ToggleFlyout();
+        }
+
+        void OnToggleMenuClose(object sender, EventArgs e)
+        {
+            if (!_isFlyoutOpen)
+            {
+                return;
+            }
+            _isFlyoutOpen = false;
+            ToggleFlyout();
+        }
+        void OnToggleMenu(object sender, EventArgs e)
+        {
+            _isFlyoutOpen = !_isFlyoutOpen;
+            ToggleFlyout();
+        }
+
+        void FlyoutClose(object sender, SwipedEventArgs e)
+        {
+            if (_isFlyoutOpen)
+                ToggleFlyout();
+        }
+
+        void FlyoutOpen(object sender, SwipedEventArgs e)
+        {
+            if (!_isFlyoutOpen)
+                ToggleFlyout();
+        }
+
+        void ToggleFlyout()
+        {
+            if (!_isFlyoutOpen)
+            {
+                Timeline.ScaleXTo(1, _flyoutSpeed);
+                Timeline.TranslateTo(0, Flyout.TranslationY, _flyoutSpeed);
+            }
+            else
+            {
+                Timeline.ScaleXTo(Scale, _flyoutSpeed);
+                Timeline.TranslateTo((Timeline.Width * -HalfScale) + Timeline.Width/2, Timeline.TranslationY, _flyoutSpeed);
+            }
+
         }
     }
 }

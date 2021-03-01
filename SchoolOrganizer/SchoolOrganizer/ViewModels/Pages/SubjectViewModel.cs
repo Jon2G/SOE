2ade630ac39c76d7cef5f36b7413db576831f28d
Jonathan Eduardo Garcia Garcia <jonyjouna@gmail.com>
2021-02-28 22:40:42 -0600

    Agregar lista de materias

--- SchoolOrganizer/SchoolOrganizer/ViewModels/Pages/SubjectViewModel.cs
+++ SchoolOrganizer/SchoolOrganizer/ViewModels/Pages/SubjectViewModel.cs
@@ -5,29 +5,38 @@ using SchoolOrganizer.Views.PopUps;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using SchoolOrganizer.Models.Scheduler;
 using Xamarin.Forms;
 
 namespace SchoolOrganizer.ViewModels.Pages
 {
-    class SubjectViewModel: BaseViewModel
+    public class SubjectViewModel : BaseViewModel
     {
-        public Command SubjectCommand { get; }
+        public List<Subject> subjects { get; }
+
         public SubjectViewModel()
         {
-            SubjectCommand = new Command(SubjectClicked);
-        }
-
-        private async void SubjectClicked(object obj)
-        {
-            var pr = new SubjectPopUp();
-            var scaleAnimation = new ScaleAnimation
-            {
-                PositionIn = MoveAnimationOptions.Right,
-                PositionOut = MoveAnimationOptions.Left
-            };
-
-            pr.Animation = scaleAnimation;
-            await PopupNavigation.Instance.PushAsync(pr);
+            subjects = new List<Subject>();
+            subjects.Add(new Subject(1, "TEORIA DE LA INFORMACION Y CODIFICACION", "#a3ddcb", "6CV1", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(2, "TEORIA DE CONTROL DIGITAL", "#e8e9a1", "6CV2", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(3, "BASES DE DATOS", "#e6b566", "6CV3", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
+            subjects.Add(new Subject(4, "SISTEMAS DE INFORMACION I", "#e5707e", "6CV4", TimeSpan.Zero, TimeSpan.Zero));
         }
     }
 }
+
+//:( hola => se murió mi teléfono, un minuto
+//meno

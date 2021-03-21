63c1e48f0a7d379d31606ba6bbc5d6b137b76472
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2021-03-21 17:43:43 -0600

    Modificaciones TaskExpander BorderColor imagen principal del Login

--- SchoolOrganizer/SchoolOrganizer/ViewModels/ViewItems/TaskFirstViewModel.cs
+++ SchoolOrganizer/SchoolOrganizer/ViewModels/ViewItems/TaskFirstViewModel.cs
@@ -18,22 +18,28 @@ namespace SchoolOrganizer.ViewModels.ViewItems
             return new ObservableCollection<Agenda>
             {
                 new Agenda { Topic = "Diagrama", Duration = "11:30 ", Color = "#B96CBD", Date = new DateTime(2020, 3, 23),
-                    Speakers = new ObservableCollection<Description>{ new Description { Name = "Base de Datos",Descripción="Diagrama sobre una tienda fisica"} } },
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Base de Datos",Descripción="Diagrama sobre una tienda fisica"} } },
 
                 new Agenda { Topic = "Proyecto", Duration = "07:30 UTC - 11:30 UTC", Color = "#49A24D", Date = new DateTime(2020, 3, 24),
-                    Speakers = new ObservableCollection<Description>{ new Description { Name = "Sistemas De Información"} } },
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Sistemas De Información"} } },
 
                 new Agenda { Topic = "Nuevas evidencias", Duration = "07:30 UTC - 11:30 UTC", Color = "#FDA838", Date = new DateTime(2020, 3, 25),
-                    Speakers = new ObservableCollection<Description>{ new Description { Name = "Administracion" }} },
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Administracion" }} },
 
                 new Agenda { Topic = "Practica2", Duration = "07:30 UTC - 11:30 UTC", Color = "#F75355", Date = new DateTime(2020, 3, 26),
-                    Speakers = new ObservableCollection<Description>{ new Description { Name = "Nuevas Tecnologias" } } },
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Nuevas Tecnologias" } } },
 
                 new Agenda { Topic = "Practica3", Duration = "07:30 UTC - 11:30 UTC", Color = "#00C6AE", Date = new DateTime(2020, 3, 27),
-                    Speakers = new ObservableCollection<Description>{ new Description { Name = "Control Digital"}} },
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Control Digital"}} },
 
                 new Agenda { Topic = "Pagina para SuperEspias", Duration = "07:30 UTC - 11:30 UTC", Color = "#455399", Date = new DateTime(2020, 3, 28),
-                    Speakers = new ObservableCollection<Description>{ new Description { Name = "Tecnologias" } } }
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Tecnologias" } } },
+                new Agenda { Topic = "Pagina para SuperEspias", Duration = "07:30 UTC - 11:30 UTC", Color = "#455399", Date = new DateTime(2020, 3, 28),
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Tecnologias" } } },
+                new Agenda { Topic = "Pagina para SuperEspias", Duration = "07:30 UTC - 11:30 UTC", Color = "#455399", Date = new DateTime(2020, 3, 28),
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Tecnologias" } } },
+                new Agenda { Topic = "Pagina para SuperEspias", Duration = "07:30 UTC - 11:30 UTC", Color = "#455399", Date = new DateTime(2020, 3, 28),
+                    Contenido = new ObservableCollection<Description>{ new Description { Name = "Tecnologias" } } }
             };
         }
     }
--- SchoolOrganizer/SchoolOrganizer/Views/Pages/TaskFirstPage.xaml.cs
+++ SchoolOrganizer/SchoolOrganizer/Views/Pages/TaskFirstPage.xaml.cs
@@ -12,51 +12,13 @@ namespace SchoolOrganizer.Views.Pages
 {
     [DesignTimeVisible(false)]
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class TaskFirstPage : ContentPage,INotifyPropertyChanged
+    public partial class TaskFirstPage : ContentPage
     {
 
-        private int _dateTime;
-        public int dateTime
-        {
-            get { return _dateTime; }
-            set
-            {
-                _dateTime = value;
-                OnPropertyChanged();
-            }
-        }
-        private int _dateTime2;
-        public int dateTime2
-        {
-            get { return _dateTime2; }
-            set
-            {
-                _dateTime2 = value;
-                OnPropertyChanged();
-            }
-        }
-        private string _mes;
-        public string mes
-        {
-            get { return _mes; }
-            set
-            {
-                _mes = value;
-                OnPropertyChanged();
-            }
-        }
+
         public TaskFirstPage()
         {
             InitializeComponent();
-            dateTime = DateTime.Now.Hour;
-            dateTime2 = DateTime.Now.Minute;
-            
-            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
-            {
-                Device.BeginInvokeOnMainThread(()=> mes=DateTime.Now.ToString("hh:mm:ss tt")
-                    );
-                return true;
-            });
         }
 
       

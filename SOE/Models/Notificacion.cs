2a61686dd7ce3a3bc2b915691bf0365ad147d506
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2021-07-07 18:50:35 -0500

    Create NotificationPage

---
+++ SOE/Models/Notificacion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOE.Models
+{
+   public  class Notificacion
+    {
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Image { get; set; }
+    }
+}
--- SOE/Views/ViewItems/NotificationView.xaml.cs
+++ SOE/Views/ViewItems/NotificationView.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SOE.Fonts;
+using SOE.Models;
+using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -18,6 +21,25 @@ namespace SOE.Views.ViewItems
         public NotificationView()
         {
             InitializeComponent();
+            notificacions = Getnotificacions();
+            this.BindingContext = this;
         }
+        public ObservableCollection<Notificacion> notificacions { get; set; }
+
+        private ObservableCollection<Notificacion> Getnotificacions()
+        {
+            return new ObservableCollection<Notificacion>
+            {
+                new Notificacion { Title = "Nueva tarea", Description = "Tarea Pendiente" ,Image="logo_soe_fill.png"},
+                new Notificacion { Title = "Nuevo tema",  Description = "Nuevo tema desbloqueado",Image="logo_soe_fill.png"},
+                new Notificacion { Title = "Nuevo logro", Description = "Obtuviste un nuevo logro",Image="logo_soe_fill.png"},
+                new Notificacion { Title = "Nuevo logro", Description = "Obtuviste un nuevo logro",Image="logo_soe_fill.png"},
+                new Notificacion { Title = "Nuevo logro", Description = "Obtuviste un nuevo logro",Image="logo_soe_fill.png"},
+                new Notificacion { Title = "Nuevo logro", Description = "Obtuviste un nuevo logro",Image="logo_soe_fill.png"},
+                new Notificacion { Title = "Nuevo logro", Description = "Obtuviste un nuevo logro",Image="logo_soe_fill.png"},
+                new Notificacion { Title = "Nuevo logro", Description = "Obtuviste un nuevo logro",Image="logo_soe_fill.png"}
+            };
+        }
+
     }
 }

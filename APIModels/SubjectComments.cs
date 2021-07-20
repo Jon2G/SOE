7cf8ad24acfed85f6b9533492287ed9610db2585
Jon2G <jonyjouna@gmail.com>
2021-07-19 20:33:44 -0500

    SubjectPageHeader

---
+++ APIModels/SubjectComments.cs
@@ -0,0 +1,20 @@
+﻿using Kit.Sql.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOEWeb.Shared
+{
+    public class SubjectComments
+    {
+        [PrimaryKey, AutoIncrement, NotNull]
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        [Ignore]
+        public string UserName { get; set; }
+        [MaxLength(140)]
+        public string Text { get; set; }
+        [Ignore]
+        public List<SubjectComments> Comments { get; set; }
+    }
+}

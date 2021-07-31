931c64b0b276b5efbea2332e7b1af5c25745c721
Santiago González Santiesteban <gonzalez.s.santiago@hotmail.com>
2021-07-30 22:01:37 -0500

    Merge

--- SOE/Views/Pages/ReminderPage.xaml.cs
+++ SOE/Views/Pages/ReminderPage.xaml.cs
@@ -1,12 +1,5 @@
 ﻿using SOE.Models;
 using SOE.ViewModels.ViewItems;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SOE.Views.Pages

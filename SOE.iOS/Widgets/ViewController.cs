9ec588dfd5cb1e283a5662a24ab30171310a3694
Jon2G <jonyjouna@gmail.com>
2021-10-11 22:50:23 -0500

    SwiftExtension Native Widget

---
+++ SOE.iOS/Widgets/ViewController.cs
@@ -0,0 +1,38 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace SOE.iOS.Widgets
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        UITextView text;
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            // Imagine this using https://docs.microsoft.com/en-us/xamarin/ios/app-fundamentals/backgrounding/ios-backgrounding-techniques/updating-an-application-in-the-background
+            // instead of just on launch
+
+            this.text = new UITextView(new CoreGraphics.CGRect(200, 200, 200, 100));
+            this.text.Text = "Before";
+            this.View.AddSubview(this.text);
+
+            NSUrl url = NSFileManager.DefaultManager.GetContainerUrl("group.com.xamarin.sample.TestApplication");
+            url = url.Append("testAppState.json", false);
+            System.IO.File.WriteAllText(url.Path, TestData.GetJson());
+
+            this.text.Text = "After";
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
